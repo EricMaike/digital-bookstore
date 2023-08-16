7105180f77d0b0eecf711c444cb27f8b2fb44518
Eric Maike <ericmaikelopes@gmail.com>
2023-08-15 21:22:39 -0400

    Instalação do React Router e configuração das rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import BookList from './components/BookList';
+import BookDetails from './components/BookDetails';
+import ShoppingCart from './components/ShoppingCart';
+import CheckoutForm from './components/CheckoutForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={BookList} />
+          <Route path="/books/:id" component={BookDetails} />
+          <Route path="/cart" component={ShoppingCart} />
+          <Route path="/checkout" component={CheckoutForm} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
